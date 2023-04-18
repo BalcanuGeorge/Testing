708128e6653eb616cc811747b31a30b1c58f01e8
GeorgeBalcanu <georgebalcanu@yahoo.com>
2023-04-18 14:41:28 +0300

    JAVA

---
+++ src/Java1.java
@@ -0,0 +1,32 @@
+
+public class Java1 {
+
+    public static void main(String[] args) {
+        //TIPURI DE DATE
+        //String
+        System.out.print("Salut,");
+        System.out.print(" eu sunt George!");
+        System.out.println();
+        System.out.println("Salut, eu sunt George!");
+        System.out.println();
+        //Numere
+        System.out.println(1);
+        System.out.println(999);
+        System.out.println();
+        //Numere Double, Float si Long
+        System.out.println(2.45);
+        System.out.println(2.45f);
+        System.out.println(999999999999L);
+        System.out.println();
+        //Valori de Adevar
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println();
+        //Caractere
+        System.out.printf("a");
+
+
+
+    }
+}
+
---
+++ src/Java2.java
@@ -0,0 +1,64 @@
+
+public class Java2 {
+
+    public static void main(String[] args) {
+
+        //VARIABILE
+        //String
+        String nume = "George";
+        System.out.println(nume);
+        System.out.println();
+        String limbajProgramare = "Java";
+        System.out.println(limbajProgramare);
+        System.out.println();
+        nume = "George Balcanu";
+        System.out.println(nume);
+        System.out.println();
+        limbajProgramare = "Python";
+        System.out.println(limbajProgramare);
+        System.out.println();
+
+        //Numar intreg
+        int numarIntreg = 100;
+        System.out.println(numarIntreg);
+        System.out.println();
+
+        //Numar Double
+        double numarDouble = 32.2;
+        System.out.println(numarDouble);
+        System.out.println();
+
+        //Numar Float
+        float numarFloat = 32.2f;
+        System.out.println(numarFloat);
+        System.out.println();
+
+        //Caractere
+        char litera ='G';
+        System.out.println(litera);
+        char cifra ='3';
+        System.out.println(cifra);
+        System.out.println();
+
+        //Valori de Adevar
+        boolean valoare1 = true;
+        System.out.println(valoare1);
+        boolean valoare2 = false;
+        System.out.println(valoare2);
+        System.out.println();
+
+        //EXERCITIU
+        int num1 =10;
+        int num2 =3;
+        int suma = num1+num2;
+        int diferenta = num1-num2;
+        int produs = num1*num2;
+        int impartire = num1/num2;
+        System.out.println(suma);
+        System.out.println(diferenta);
+        System.out.println(produs);
+        System.out.println(impartire);
+
+    }
+
+}
