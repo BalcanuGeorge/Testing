a89846d7d44fbe1d674c7190798f92d91e5adf08
GeorgeBalcanu <georgebalcanu@yahoo.com>
2023-04-19 14:23:08 +0300

    JAVA

---
+++ src/Java3.java
@@ -0,0 +1,72 @@
+
+public class Java3 {
+
+    public static void main(String[] args) {
+        //OPERATORI MATEMATICI
+        //+ - * / %
+        System.out.println(100+100);
+        System.out.println(100-50);
+        System.out.println(100*3);
+        System.out.println(100/4);
+        System.out.println(10%3);
+        System.out.println();
+
+        //OPERATORI RATIONALI
+        System.out.println(7==7);
+        System.out.println(7==8);
+        System.out.println(7!=7);
+        System.out.println(7!=8);
+        System.out.println(3+3 < 10);
+        System.out.println(3+3 > 10);
+        System.out.println(3>=4);
+        System.out.println(3<=4);
+        System.out.println();
+
+        //OPERATORI LOGICI
+        System.out.println(3>2 && 3 >1);
+        System.out.println(3==3 || 3<2);
+        System.out.println(!true);
+        System.out.println(!false);
+        System.out.println();
+
+        //OPERATORI DE ATRIBUIRE
+        int numar = 5;
+        numar++;
+        System.out.println(numar);
+
+        int numar2 =5;
+        numar--;
+        System.out.println(numar2);
+
+
+        int numar3 = 10;
+        numar3 +=20;
+        System.out.println(numar3);
+
+        int numar4 = 10;
+        numar4 -=5;
+        System.out.println(numar4);
+
+        int numar5 = 10;
+        numar5 /=2;
+        System.out.println(numar5);
+
+        int numar6 = 10;
+        numar6 *=2;
+        System.out.println(numar6);
+
+        numar = numar +15;
+        System.out.println(numar);
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
---
+++ src/Java4.java
@@ -0,0 +1,73 @@
+
+public class Java4 {
+
+    public static void main(String[] args) {
+
+        //CONCATENARE
+        int numar =10;
+        String text = "George";
+        double numarDouble = 10.1;
+        float numarFloat = 10.1f;
+        boolean adevar = true;
+        char c = '*';
+
+        System.out.println("Numarul este " +numar);
+        System.out.println("Variabila mea String are valoarea " +text);
+        System.out.println("Valoarea Double este " +numarDouble);
+        System.out.println("Valoarea Float este " +numarFloat);
+        System.out.println("Booleanul este " +adevar);
+        System.out.println("Caracterul este " +c);
+        System.out.println();
+
+        System.out.printf("Numarul meu este %d", numar);
+        System.out.println();
+        System.out.printf("Variabila mea String are valoarea %s", text);
+        System.out.println();
+        System.out.printf("Valoarea Double este %f", numarDouble);
+        System.out.println();
+        System.out.printf("Booleanul este %b", adevar);
+        System.out.println();
+        System.out.printf("Caracterul meu este %c", c);
+        System.out.println();
+        System.out.printf("Numarul meu este %d iar variabila mea String este %s", numar, text);
+        System.out.println();
+
+
+        //EXERCITII
+        //
+        int n1 = 10;
+        int n2 = 5;
+        int n3 = 7;
+        System.out.printf("Primul numar este %d, al doilea este %d, al treilea este %d", n1, n2, n3);
+        System.out.println();
+
+
+        //
+        int suma = n1+n2;
+        int diferenta = n1-n2;
+        int produs = n1*n2;
+        int impartire = n1/n2;
+        System.out.println("Suma este " +suma);
+        System.out.println("Diferenta este " +diferenta);
+        System.out.println("Produsul este " +produs);
+        System.out.println("Impartirea este " +impartire);
+        System.out.println();
+
+
+        //
+        int n4 =1000;
+        int n5=270;
+        int restulImpartirii = n4%n5;
+        System.out.println("Restul impartirii lui n4 la n5 este " +restulImpartirii);
+        System.out.printf("Restul impartirii lui %d la %d este egal cu %d", n4, n5, restulImpartirii);
+        System.out.println();
+
+
+
+
+
+
+    }
+}
+
+
